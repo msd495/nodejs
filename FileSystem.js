d00d66cef707d864ea98b634c1a6afe91c529e2f
Ayush495 <msd495@gmail.com>
2021-07-04 14:59:04 +0530

    Update FileSystem.js

--- FileSystem.js
+++ FileSystem.js
@@ -1,56 +1,41 @@
-var fs = require("fs");
+let fs = require("fs");
 
-console.log("Write data into filesssss");
-const dataToWrite='Hey ,how are you guys??';
-const callbackWrite=function(err) {
-   if (err) {
-      return console.error(err);
-   }
-   
-   console.log("Data written successfully!");
-   //console.log("Let's read newly written data");
- 
-};
-fs.writeFile('input.txt',dataToWrite , callbackWrite );
+class FileSystem{
+    constructor(fileName) {
+        this._fileName = fileName
+    }
+    //setter method
+    set contentOfFile(data){
+        this._data = data;
+    }
+    //getter method
+    get contentFromFile(){
+        return this._data;
+    }
+    readFromFile(){
+        //add try catch
+        fs.readFile(this._fileName,(err,data)=>{
+            if (!err){
+                console.log(`Contents of file : ${data.toString()}`)
+            }
+        })
+    }
+    writeIntoFile(){
+        //add try ctach
+        fs.writeFile(this._fileName,this.contentFromFile,(err,data)=>{
+            if (err){
+                console.log("file not present")
+            }
+            console.log(`Data written to file`)
+        })
+    }
+
+}
 
 
-//reading from a file
-let callback=function (err, data) {
-  if (err) {
-     return console.error(err);
-  }
-  console.log("Read file asynchronously: " + data.toString());
-};
-fs.readFile('input.txt',callback);//20 mins
-console.log("ready");
+fileObj = new FileSystem('./sample.txt')
+fileObj.contentOfFile = "Hello, how are you?"
+fileObj.writeIntoFile()
+fileObj.readFromFile()
 
-  //reading file synchronysly
-  var data = fs.readFileSync( 'input.txt');
-  console.log( data.toString() );
-  console.log( "Do other tasks");
-
-   
-   
-/*
-console.log("Going to create directory D://Node.js-zenrys//NodeApp//test");
-fs.mkdir('D://Node.js-zenrys//NodeApp//test',function(err){
-   if (err) {
-      return console.error(err);
-   }
-   console.log("Directory created successfully!");
-});
-*/
-
-/*
-
-console.log("Going to read directory D://Node.js-zenrys//NodeApp//");
-fs.readdir("D://Node.js-zenrys//NodeApp//",function(err, files){
-   if (err) {
-      return console.error(err);
-   }
-   files.forEach( function (file){
-      console.log( file );
-   });
-});
-
-*/
+console.log(`End of program`);
